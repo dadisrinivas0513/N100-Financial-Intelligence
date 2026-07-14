07d1cdb47598f4f0817f7cfebdb6475c6b497dfe
Dadi Srinivas <dadisrinivas93911@gmail.com>
2026-07-14 22:57:05 +0530

    Final Sprint 1 changes

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,39 +4,28 @@ pipeline.py
 Main ETL Pipeline
 """
 
-import logging
-from pathlib import Path
-from src.utils.logger import logger
 import subprocess
 import sys
+from pathlib import Path
+
+from src.utils.logger import logger
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
-
-LOG_FOLDER = PROJECT_ROOT / "logs"
-LOG_FOLDER.mkdir(exist_ok=True)
-
-LOG_FILE = LOG_FOLDER / "pipeline.log"
-
-logging.basicConfig(
-    filename=LOG_FILE,
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s"
-)
 
 
 def run(script):
 
-    logging.info(f"Running {script}")
+    logger.info(f"Running {script}")
 
     result = subprocess.run(
-        [sys.executable, script]
+        [sys.executable, str(PROJECT_ROOT / script)]
     )
 
     if result.returncode == 0:
-        logging.info(f"{script} SUCCESS")
+        logger.info(f"{script} SUCCESS")
     else:
-        logging.error(f"{script} FAILED")
-        exit()
+        logger.error(f"{script} FAILED")
+        sys.exit()
 
 
 print("=" * 80)
@@ -54,4 +43,4 @@ print("\n" + "=" * 80)
 print("PIPELINE COMPLETED SUCCESSFULLY")
 print("=" * 80)
 
-logging.info("Pipeline Finished Successfully")
+logger.info("Pipeline Finished Successfully")
